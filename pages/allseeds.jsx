162050b394978a56cfda8aaa7a71ccfdd3644860
miahbates <miahbates@icloud.com>
2022-03-21 14:46:38 +0000

    filter page components done
    Co-authored-by: Milly <62570785+millipede-cpu@users.noreply.github.com>
    Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>

--- pages/allseeds.jsx
+++ pages/allseeds.jsx
@@ -4,6 +4,7 @@ import CategoryFilters from "../components/categoryFilters";
 import SearchBar from "../components/searchBar";
 import ConditionFilters from "../components/conditionFilters";
 import PriceFilters from "../components/priceFilters";
+import ProductDisplay from "../components/productDisplay";
 
 export default function AllSeeds() {
   return (
@@ -12,6 +13,7 @@ export default function AllSeeds() {
       <SearchBar></SearchBar>
       <ConditionFilters></ConditionFilters>
       <PriceFilters></PriceFilters>
+      <ProductDisplay></ProductDisplay>
     </div>
   );
 }
