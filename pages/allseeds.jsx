1ee151aef64a8b3e1135e0ff0a27e5e5c1cc92cc
orian <77785778+OrianP@users.noreply.github.com>
2022-03-21 17:35:39 +0000

    Add console.log of products variable
    Verify that dummy data has been added to the project
    
    Relates #4

--- pages/allseeds.jsx
+++ pages/allseeds.jsx
@@ -5,8 +5,10 @@ import SearchBar from "../components/searchBar";
 import ConditionFilters from "../components/conditionFilters";
 import PriceFilters from "../components/priceFilters";
 import ProductDisplay from "../components/productDisplay";
+import products from "../database/dummy-data";
 
 export default function AllSeeds() {
+  console.log(products);
   return (
     <div>
       <CategoryFilters></CategoryFilters>
