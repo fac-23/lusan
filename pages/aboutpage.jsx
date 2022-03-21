98e1056821e3b3d345c4f93ab65c9456fca6694a
MillyP <millypalmer1@hotmail.co.uk>
2022-03-21 18:37:36 +0000

    Created About Page

---
+++ pages/aboutpage.jsx
@@ -0,0 +1,25 @@
+// import Head from "next/head";
+// import Image from "next/image";
+// import StyledButton from "../components/styled-components/StyledButton";
+// import Layout from "../components/layout";
+// import StyledLink from "../components/styled-components/StyledLink";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSeedling,
+  faHandHoldSeedling,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <h2>Our Values</h2>
+      <FontAwesomeIcon icon={faSeedling}></FontAwesomeIcon>
+      <FontAwesomeIcon icon={faHandHoldSeedling}></FontAwesomeIcon>
+      <Link href={"/"}>
+        <a>⏎ Back to home</a>
+      </Link>
+    </div>
+  );
+}
