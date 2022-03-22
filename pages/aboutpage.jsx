6131d80e8e72b9cc9f8bbba3cc7325cde866d825
MillyP <millypalmer1@hotmail.co.uk>
2022-03-22 10:00:34 +0000

    Commented out about-page as dynamic route not made yet

--- pages/aboutpage.jsx
+++ pages/aboutpage.jsx
@@ -3,19 +3,19 @@
 // import StyledButton from "../components/styled-components/StyledButton";
 // import Layout from "../components/layout";
 // import StyledLink from "../components/styled-components/StyledLink";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/fontawesome-svg-core";
-import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+// import Link from "next/link";
+// import { FontAwesomeIcon } from "@fortawesome/fontawesome-svg-core";
+// import { faSeedling } from "@fortawesome/free-solid-svg-icons";
 
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <h2>Our Values</h2>
-      <FontAwesomeIcon icon={faSeedling} className="icon"></FontAwesomeIcon>
-      <Link href={"/"}>
-        <a>⏎ Back to home</a>
-      </Link>
-    </div>
-  );
-}
+// export default function AboutPage() {
+//   return (
+//     <div>
+//       <h1>About Us</h1>
+//       <h2>Our Values</h2>
+//       <FontAwesomeIcon icon={faSeedling} className="icon"></FontAwesomeIcon>
+//       <Link href={"/"}>
+//         <a>⏎ Back to home</a>
+//       </Link>
+//     </div>
+//   );
+// }
